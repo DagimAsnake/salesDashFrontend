10af282a0ca89c0e6fd5fe8fd3d05582e3bfcdb7
Dagim Asnake <dagimasnake1@gmail.com>
2023-06-01 04:13:36 -1200

    added search and filter to products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Card, Input, Select } from "antd";
 import AdminAuthContext from "../components/store/Admin-authContext";
+
+const { Search } = Input;
+const { Option } = Select;
 
 function Products() {
     const adminAuthCtx = useContext(AdminAuthContext);
 
     const [requestEmployee, setRequestEmployee] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [filterValue, setFilterValue] = useState("");
+    const [productTypes, setProductTypes] = useState([])
 
     useEffect(() => {
         const fetchEmployee = async () => {
@@ -24,35 +31,99 @@ function Products() {
         fetchEmployee();
     }, [adminAuthCtx]);
 
+    useEffect(() => {
+        const types = new Set(requestEmployee.map((product) => product.productType));
+        setProductTypes(Array.from(types));
+    }, [requestEmployee]);
+
+    const handleSearch = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
+    const handleFilter = (value) => {
+        setFilterValue(value);
+
+    };
+
+    const filteredProducts = requestEmployee.filter((product) => {
+        if (!filterValue) {
+            return true;
+        }
+        return product.productType === filterValue;
+    });
+
+    const searchedProducts = filteredProducts.filter((product) => {
+        if (!searchQuery) {
+            return true;
+        }
+        return (
+            product.productName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            product.productType.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    });
+
     return (
         <>
-            {isLoading && <div className="flex">
-                <div className="m-auto">
-                    <div className="inline-flex items-center justify-center">
-                        <div className="h-8 w-8 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
-                        <div className="h-8 w-8 ml-4 border-t-2 border-b-2 border-gray-900 rounded-full animate-ping"></div>
+            <div className="flex justify-center my-4">
+                <Search
+                    placeholder="Search products"
+                    allowClear
+                    enterButton
+                    onChange={handleSearch}
+                    className="mr-4"
+                />
+                <Select
+                    defaultValue=""
+                    style={{ width: 120 }}
+                    onChange={handleFilter}
+                    className="mr-4"
+                >
+                    <Option value="">All</Option>
+                    {productTypes.map(type => (
+                        <Option key={type} value={type}>{type}</Option>
+                    ))}
+                </Select>
+            </div>
+            {isLoading && (
+                <div className="flex">
+                    <div className="m-auto">
+                        <div className="inline-flex items-center justify-center">
+                            <div className="h-8 w-8 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+                            <div className="h-8 w-8 ml-4 border-t-2 border-b-2 border-gray-900 rounded-full animate-ping"></div>
+                        </div>
                     </div>
                 </div>
-            </div>}
+            )}
             <div className="grid grid-cols-4 gap-4 mx-auto">
-                {!isLoading && requestEmployee.length <= 0 && (
-                    <h3 className="mt-5">There are no Products.</h3>)}
-                {!isLoading && requestEmployee.map((emp) => {
-                    return (
-                        <div key={emp._id} className="bg-white rounded-lg shadow-lg overflow-hidden max-w-xs">
-                            <div className="px-4 py-2">
-                                <h2 className="text-xl font-bold text-gray-800 mb-2">{emp.productName}</h2>
-                                <p className="text-sm text-gray-600 mb-2">Type: {emp.productType} </p>
-                                <p className="text-lg text-gray-900 font-bold mb-2">${emp.cost}</p>
-                                <Link to={`/product/${emp._id}`}>
-                                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                        Read more ...
-                                    </button>
-                                </Link>
+                {!isLoading && searchedProducts.length <= 0 && (
+                    <h3 className="mt-5">There are no Products.</h3>
+                )}
+                {!isLoading &&
+                    searchedProducts.map((product) => {
+                        return (
+                            <div key={product._id}>
+                                <Card className="max-w-xs">
+                                    <Link to={`/product/${product._id}`}>
+                                        <Card.Meta
+                                            title={product.productName}
+                                            description={`Type: ${product.productType}`}
+                                            className="mb-2"
+                                        />
+                                    </Link>
+                                    <div className="flex justify-between">
+                                        <p className="text-lg text-gray-900 font-bold mb-2">
+                                            ${product.cost}
+                                        </p>
+                                        <Link to={`/product/${product._id}`}>
+                                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                                Read more ...
+                                            </button>
+                                        </Link>
+                                    </div>
+                                </Card>
                             </div>
-                        </div>
-                    );
-                })}
+                        );
+                    })}
             </div>
         </>
     );
